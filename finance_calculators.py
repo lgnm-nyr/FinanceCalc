c125d73ad1cab43574dd68fd21b8a95af1c09c01
lgnm-nyr <a.mangal.work@gmail.com>
2024-03-31 20:14:03 +0100

    formatted line lengths

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,6 +1,6 @@
 import math
 
-#Functions to make the breaks for easier reading
+# Functions to make the breaks for easier reading
 def start_speech():
     print('=' * 150)
     print()
@@ -11,7 +11,7 @@ def end_speech():
 
 
 
-#Making functions so that it becomes easier to restart when someone misspells something
+# Making functions so that it becomes easier to restart when someone misspells something
 def intro():
 
     #general opening text and choosing an option
@@ -39,30 +39,34 @@ def intro():
 
 
 
-#Function for the investment system
+# Function for the investment system
 def investment():
-        
-    #finding out all the required numbers to calculate with
+
+    # finding out all the required numbers to calculate with
     start_speech()
-    deposit = float(input("Enter the amount of money you are depositing to the nearest pounds(£): "))
-    interest_rate = float(input("Enter the interest rate at which you wish to grow your money at in %: "))
+    deposit = float(input('''Enter the amount of money you are depositing"
+                          to the nearest pound(£): '''))
+    interest_rate = float(input('''Enter the interest rate at which
+                                you wish to grow your money at in %: '''))
     num_years = float(input("Enter the amount of years you plan to invest for: "))
     interest_type = input("Enter the type of interest you would like to use (Simple or Compound): ")
     end_speech()
 
-    #Formatting the variables so they can be used in the upcoming calculations - variables changed so that they fit mathmatical formulas and so it is quicker to type out
+    # Formatting the variables so they can be used in the upcoming calculations
+    # - variables changed so that they fit mathmatical formulas and so it is quicker to type out
     p = deposit
     r = interest_rate/100
     t = num_years
     interest_type = interest_type.lower()
-    
+
     if interest_type == "simple":
         #Maths
         total = p * (1 + (r * t))
-        
+
         #Result(rounded to 2dp so it makes sense money wise)
         start_speech()
-        print(f"The total amount of money £{deposit} becomes after {num_years} years with a simple interest rate of {interest_rate}% is £{round(total,2)}")
+        print(f'''The total amount of money £{deposit} becomes after {num_years} years
+              with a simple interest rate of {interest_rate}% is £{round(total,2)}''')
         end_speech()
 
         #More calcs?
@@ -71,10 +75,11 @@ def investment():
     elif interest_type == "compound":
         #Maths
         total = p * (math.pow((1 + r), t))
-        
+
         #Result(rounded to 2dp so it makes sense money wise)
         start_speech()
-        print(f"The total amount of money £{deposit} becomes after {num_years} years with a compound interest rate of {interest_rate}% is £{round(total,2)}")
+        print(f'''The total amount of money £{deposit} becomes after {num_years} years
+              with a compound interest rate of {interest_rate}% is £{round(total,2)}''')
         end_speech()
 
         #More calcs?
@@ -82,7 +87,7 @@ def investment():
 
     else:
         #Error msg
-        start_speech()  
+        start_speech()
         print("You can only pick Simple or Compound types of investment")
         end_speech()
 
@@ -91,27 +96,32 @@ def investment():
 
 
 
-#Function for bond system   
+#Function for bond system
 def bond():
 
     #Number inputs
     start_speech()
-    house_value = float(input("Enter the current value of the house you wish to assess in pounds(£): "))
-    interest_rate = float(input("Enter the yearly interest rate at which your bond will be applied with in %: "))
-    num_months = float(input("Enter the number of number of months you wish to take to repay the bond: "))
+    house_value = float(input('''Enter the current value of the house
+                              you wish to assess in pounds(£): '''))
+    interest_rate = float(input('''Enter the yearly interest rate at
+                                which your bond will be applied with in %: '''))
+    num_months = float(input('''Enter the number of number of months
+                             you wish to take to repay the bond: '''))
     end_speech()
 
-    #Fomratting numbers so that calculations work properly, also providing different names to fit mathmatical formulae and to type it quicker
+    # Fomratting numbers so that calculations work properly,
+    # also providing different names to fit mathmatical formulae and to type it quicker
     p = house_value
     i = (interest_rate/100)/12
     n = num_months
-    
+
     #Maths
     repayment = (i * p)/(1 - (1 + i)**(-n))
-    
+
     #Result(rounded to 2dp so it makes sense money wise)
     start_speech()
-    print(f'''On a house of present value £{house_value}, a bond of {num_months} months with a yearly interest rate of {interest_rate}%,
+    print(f'''On a house of present value £{house_value},
+          a bond of {num_months} months with a yearly interest rate of {interest_rate}%,
           \nYou would have to repay £{round(repayment,2)} every month''')
     end_speech()
 
@@ -125,7 +135,8 @@ def ending():
 
     #General message
     start_speech()
-    restart_option = input('''If you would like to calculate another investment or bond, please enter \"restart!\"
+    restart_option = input('''If you would like to calculate another investment or bond,
+                           please enter \"restart!\"
                            \nOtherwise, please enter anything else
                            \n''')
     end_speech()
